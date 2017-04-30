f8aac209d7746647e4d50c8a15766f8b7574f299
Kimberly Soares <kimberly.soares02@gmail.com>
2017-04-30 23:49:01 +0000

    updated init file

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './app/static/uploads' # location where file uploads will be stored
-app.config['SECRET_KEY'] = os.urandom(24) # used for session security
+#app.config['SECRET_KEY'] = os.urandom(24) # used for session security
+app.config['SECRET_KEY'] = '$hhh!'
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:password@localhost/project2" # database setup
 #DATABASE_URL='postgres://rytmloidwripgk:1f1c20a3b1e7b951b95b07cbc375903f9d16ddc6225789b82c097c6f64265385@ec2-54-235-168-152.compute-1.amazonaws.com:5432/d35ul9km490k63'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://rytmloidwripgk:1f1c20a3b1e7b951b95b07cbc375903f9d16ddc6225789b82c097c6f64265385@ec2-54-235-168-152.compute-1.amazonaws.com:5432/d35ul9km490k63" # database setup
